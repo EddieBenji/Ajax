229160015f4b9209a865897952228195ecb53c93
Lalo <eduardo_benji@hotmail.com>
2016-03-02 19:08:46 -0600

    Jquery en ajax

---
+++ jQuery/index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lalo
+ * Date: 02/03/2016
+ * Time: 06:38 PM
+ */
+
+?>
+
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Página</title>
+</head>
+<body>
+<h1>Resultados, goles y más</h1>
+omokmom
+<br/>
+mokml
+<br/>
+hola
+<br/>
+aiusdiuasd
+</body>
+</html>
