661dc6c02e1568a1fe694309356918fd427f8e3d
Lalo <eduardo_benji@hotmail.com>
2016-03-07 19:26:28 -0600

    Se agregan más animaciones, interacciones y estilos

--- jQuery/Practica jQuery ui/js/my-app.js
+++ jQuery/Practica jQuery ui/js/my-app.js
@@ -12,10 +12,17 @@ $(function () {
 
 function runEffects() {
     var divEffect = $('#effect');
-    divEffect.effect('bounce', 'slow', 500);
-    divEffect.animate({
-        backgroundColor: 'red',
-        color: 'blue',
-        width: 500
-    }, 1000);
+    //divEffect.effect('bounce', 'slow', 500);
+    //divEffect.animate({
+    //    backgroundColor: 'green',
+    //    color: 'blue',
+    //    width: 500
+    //}, 1000);
+//    divEffect.effect('scale', {percent: 0}, 500);
+//    divEffect.effect('size', {to: {width: 200, height: 60}}, 500);
+    divEffect.effect('transfer', {to: '#btn-trigger', className: 'ui-effects-transfer'}, 500);
+
+    //Interactions with jquery
+    //divEffect.draggable();
+    divEffect.draggable();
 }
